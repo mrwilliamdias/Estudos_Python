8c24aca69db45142e6bdc454085f100b6e187bef
william <mrwilliamdias@gmail.com>
2020-12-14 22:42:12 -0300

    Tratando Dados e Fazendo contas - Aula 6

---
+++ Python3-Mundo1/Tratando dados e fazendo contas/Aula_6/teste_A.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite o valor de N1: '))
+n2 = int(input('Digite o valor de N2: '))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
+print(type(n1))
+print(type(n2))
+print(type(s))
