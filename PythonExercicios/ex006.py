f1c6d7692f9cec844eb15b4b428c8e71d0550527
william <mrwilliamdias@gmail.com>
2020-12-16 14:23:36 -0300

    Exercícios 6 e 7 - Dobro, Tripl, Raiz Quadrada e Média

---
+++ PythonExercicios/ex006.py
@@ -0,0 +1,5 @@
+a = int(input('Informe o Valor de A: '))
+print('Valor de A: {}\n'
+      'Dobro de A: {}\n'
+      'Triplo de A: {}\n'
+      'Raiz Quadrada de A: {:.2f}'.format(a, a * 2, a * 3, a ** (1/2)))
---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,6 @@
+a1 = float(input('Digite a Nota da Avaliação A1: '))
+a2 = float(input('Digite a Nota da Avaliação A2: '))
+m = (a1 + a2) / 2
+print('Nota A1: {:.2f}\n'
+      'Nota A2: {:.2f}\n'
+      'Média Final: {:.2f}\n'.format(a1, a2, m))
